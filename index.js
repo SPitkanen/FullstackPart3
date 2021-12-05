e92955cb8d8aee66accea2cffcd5e00fbd3c67c7
Santeri Pitkänen <santeri.pitkanen@me.com>
2021-12-05 17:08:16 +0200

    uibuild

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json())
 morgan.token('data', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
